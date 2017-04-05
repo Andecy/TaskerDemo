ee417171c040dbda706fa37fae54e60dcc09ba70
罗驰正 <andecy@foxmail.com>
2017-04-05 16:44:39 +0800

    加入单选题的例子

--- tasker/src/main/java/com/lancoo/tasker/adapter/SimpleItemAdapter.java
+++ tasker/src/main/java/com/lancoo/tasker/adapter/SimpleItemAdapter.java
@@ -1,13 +1,13 @@
 package com.lancoo.tasker.adapter;
 
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 
 import com.lancoo.tasker.module.TaskData;
 import com.lancoo.tasker.module.answer.ItemAnswer;
 import com.lancoo.tasker.module.timu.ItemTimu;
 import com.lancoo.tasker.ui.BaseItemFragment;
 import com.lancoo.tasker.ui.DefaultFillBlankFragment;
+import com.lancoo.tasker.ui.DefaultSingleChoiceFragment;
 
 /**
  * Author: Andecy
@@ -26,7 +26,12 @@ public class SimpleItemAdapter extends BaseItemAdapter {
     @Override
     protected BaseItemFragment getItemByType(ItemTimu itemTimu, ItemAnswer itemAnswer) {
 
-        return DefaultFillBlankFragment.newInstance(true, itemTimu, itemAnswer);
+        switch ((int) (Math.random() * 10) % 2) {
+            case 0:
+                return DefaultFillBlankFragment.newInstance(true, itemTimu, itemAnswer);
+            default:
+            case 1:
+                return DefaultSingleChoiceFragment.newInstance(true, itemTimu, itemAnswer);
+        }
     }
-
 }
